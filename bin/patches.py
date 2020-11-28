b3b2b4cff2cefbffd3ecedace42083d0e2d425b0
devforfu <developer.z@outlook.com>
2020-11-29 00:02:45 +0100

    Cutting patches

--- bin/patches.py
+++ bin/patches.py
@@ -4,10 +4,11 @@ import os
 from argparse import ArgumentParser, Namespace
 from functools import partial
 from logging import getLogger
-from typing import Generator, Dict, List
+from os.path import join
+from typing import Dict
 
+import PIL.Image
 import dask.bag as db
-import numpy as np
 from distributed import Client
 
 from kidney.datasets.kaggle import KaggleKidneyDatasetReader, SampleType, DatasetReader
@@ -28,10 +29,9 @@ def main(args: Namespace):
         bag = (
             db.from_sequence(keys, npartitions=len(keys))
             .map(partial(read_from_disk, reader=reader))
-            .map(partial(generate_patches, size=args.patch_stride, stride=args.patch_size))
-            .map(partial(save_on_disk, output_dir=args.output_dir))
+            .map(partial(generate_patches, size=args.patch_stride, stride=args.patch_size, output_dir=args.output_dir))
         )
-        results = bag.compute(scheduler='sync')
+        bag.compute(scheduler='synchronous')
     finally:
         client.close()
 
@@ -42,8 +42,9 @@ def read_from_disk(key: str, reader: DatasetReader) -> Dict:
     return sample
 
 
-def generate_patches(sample: Dict, size: int, stride: int) -> List[Dict]:
-    # image, mask, key = sample['image'], sample['mask'], sample['key']
+def generate_patches(sample: Dict, size: int, stride: int, output_dir: str) -> Dict:
+    os.makedirs(output_dir, exist_ok=True)
+
     image = sample['image']
     key = sample['key']
     mask = sample.get('mask')
@@ -53,57 +54,25 @@ def generate_patches(sample: Dict, size: int, stride: int) -> List[Dict]:
             image.shape[0] != mask.shape[0] or
             image.shape[1] != mask.shape[1]
         ):
-            return [{'key': key, 'error': 'image and mask shape do not match'}]
+            return {'key': key, 'error': 'image and mask shape do not match'}
 
         if mask.ndim != 2:
-            return [{'key': key, 'error': 'mask is not binary'}]
+            return {'key': key, 'error': 'mask is not binary'}
 
-    y_size, x_size = image.shape[:1]
-
-    patches = []
+    y_size, x_size = image.shape[:2]
 
     # todo: consider padding edge patches instead of ignoring them
     for dy in range(0, y_size, stride):
         for dx in range(0, x_size, stride):
-            result = dict(key=key, box=[dx, dy, dx + size, dy + size])
-            result['patch_image'] = image[dy:dy + size, dx:dx + size]
+
+            path = join(output_dir, f'img.{dx}.{dy}.{size}.png')
+            image = PIL.Image.fromarray(image[dy:dy + size, dx:dx + size])
+            image.save(path, format='png')
+
             if mask is not None:
-                result['patch_mask'] = mask[dy:dy + size, dx:dx + size]
-            #yield {'image': patch_image, 'mask': patch_mask}
-            yield patches
-
-
-def save_on_disk(patch: Dict, output_dir: str) -> Dict:
-    return {}
-
-
-# for key in keys:
-#     _logger.info('processing key: %s', key)
-#     sample = reader.fetch_one(key)
-#     for image, mask in generate_patches(
-#         **sample,
-#         stride=args.stride,
-#         size=args.patch_size,
-#         drop_last=args.drop_last
-#     ):
-#         pass
-
-
-# def generate_patches(
-#     sample: np.ndarray,
-#     mask: np.ndarray,
-#     stride: int,
-#     size: int,
-#     drop_last: bool = True  # unused
-# ) -> Generator:
-#     assert sample.shape[:1] == mask.shape[:1], 'sample and mask shape should match'
-#     assert mask.ndim == 2, 'mask should be binary'
-#
-#     y_size, x_size = sample.shape[0], sample.shape[1]
-#     for dx in range(0, x_size, stride):
-#         for dy in range(0, y_size, stride):
-#             yield
-#
+                path = join(output_dir, f'seg.{dx}.{dy}.{size}.png')
+                image = PIL.Image.fromarray(mask[dy:dy + size, dx:dx + size])
+                image.save(path, format='png')
 
 
 def parse_args() -> Namespace:
@@ -114,9 +83,10 @@ def parse_args() -> Namespace:
     parser.add_argument('--patch-stride', default=512)
     parser.add_argument('--drop-last', action='store_true')
     parser.add_argument('--sample-type',
-                        choices=[case.__name__ for case in SampleType],
-                        type=SampleType)
+                        choices=[case.name for case in SampleType],
+                        default=SampleType.Labeled.name)
     args = parser.parse_args()
+    args.sample_type = SampleType[args.sample_type]
     return args
 
 
