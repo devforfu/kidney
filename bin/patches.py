27c951bf2e04179a7493258f11877b1733902dc7
devforfu <developer.z@outlook.com>
2020-11-28 23:31:00 +0100

    [WIP] Cutting patches

---
+++ bin/patches.py
@@ -0,0 +1,124 @@
+"""Reads TIFF images and cuts them into smaller pieces ready for model training."""
+
+import os
+from argparse import ArgumentParser, Namespace
+from functools import partial
+from logging import getLogger
+from typing import Generator, Dict, List
+
+import dask.bag as db
+import numpy as np
+from distributed import Client
+
+from kidney.datasets.kaggle import KaggleKidneyDatasetReader, SampleType, DatasetReader
+
+_logger = getLogger(__name__)
+
+
+def main(args: Namespace):
+    _logger.info('reading samples from dir: %s', args.input_dir)
+    reader = KaggleKidneyDatasetReader(args.input_dir)
+
+    _logger.info('sample type: %s', args.sample_type)
+    keys = reader.get_keys(args.sample_type)
+    _logger.info('retrieved keys: %s', ', '.join(keys))
+
+    client = Client()
+    try:
+        bag = (
+            db.from_sequence(keys, npartitions=len(keys))
+            .map(partial(read_from_disk, reader=reader))
+            .map(partial(generate_patches, size=args.patch_stride, stride=args.patch_size))
+            .map(partial(save_on_disk, output_dir=args.output_dir))
+        )
+        results = bag.compute(scheduler='sync')
+    finally:
+        client.close()
+
+
+def read_from_disk(key: str, reader: DatasetReader) -> Dict:
+    sample = reader.fetch_one(key)
+    sample['key'] = key
+    return sample
+
+
+def generate_patches(sample: Dict, size: int, stride: int) -> List[Dict]:
+    # image, mask, key = sample['image'], sample['mask'], sample['key']
+    image = sample['image']
+    key = sample['key']
+    mask = sample.get('mask')
+
+    if mask is not None:
+        if (
+            image.shape[0] != mask.shape[0] or
+            image.shape[1] != mask.shape[1]
+        ):
+            return [{'key': key, 'error': 'image and mask shape do not match'}]
+
+        if mask.ndim != 2:
+            return [{'key': key, 'error': 'mask is not binary'}]
+
+    y_size, x_size = image.shape[:1]
+
+    patches = []
+
+    # todo: consider padding edge patches instead of ignoring them
+    for dy in range(0, y_size, stride):
+        for dx in range(0, x_size, stride):
+            result = dict(key=key, box=[dx, dy, dx + size, dy + size])
+            result['patch_image'] = image[dy:dy + size, dx:dx + size]
+            if mask is not None:
+                result['patch_mask'] = mask[dy:dy + size, dx:dx + size]
+            #yield {'image': patch_image, 'mask': patch_mask}
+            yield patches
+
+
+def save_on_disk(patch: Dict, output_dir: str) -> Dict:
+    return {}
+
+
+# for key in keys:
+#     _logger.info('processing key: %s', key)
+#     sample = reader.fetch_one(key)
+#     for image, mask in generate_patches(
+#         **sample,
+#         stride=args.stride,
+#         size=args.patch_size,
+#         drop_last=args.drop_last
+#     ):
+#         pass
+
+
+# def generate_patches(
+#     sample: np.ndarray,
+#     mask: np.ndarray,
+#     stride: int,
+#     size: int,
+#     drop_last: bool = True  # unused
+# ) -> Generator:
+#     assert sample.shape[:1] == mask.shape[:1], 'sample and mask shape should match'
+#     assert mask.ndim == 2, 'mask should be binary'
+#
+#     y_size, x_size = sample.shape[0], sample.shape[1]
+#     for dx in range(0, x_size, stride):
+#         for dy in range(0, y_size, stride):
+#             yield
+#
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument('--input-dir', required=True)
+    parser.add_argument('--output-dir', required=True)
+    parser.add_argument('--patch-size', default=512)
+    parser.add_argument('--patch-stride', default=512)
+    parser.add_argument('--drop-last', action='store_true')
+    parser.add_argument('--sample-type',
+                        choices=[case.__name__ for case in SampleType],
+                        type=SampleType)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    main(parse_args())
