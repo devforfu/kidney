94cfd1ac769e076f912dd12c69db6fe11d9a38c2
devforfu <developer.z@outlook.com>
2020-12-16 21:51:38 +0100

    Image vs gt vs pred preview

--- kidney/utils/plotting.py
+++ kidney/utils/plotting.py
@@ -1,10 +1,11 @@
-from typing import Optional, Union, Tuple, Dict
+from typing import Optional, Union, Tuple, Dict, cast
 
 import numpy as np
 from matplotlib.axis import Axis
+from monai.utils import issequenceiterable
 from zeus.plotting.utils import axes
 
-from kidney.utils.image import read_masked_image
+from kidney.utils.image import read_masked_image, overlay
 from kidney.utils.mask import rle_decode
 
 
@@ -13,7 +14,7 @@ def plot_mask(
     enumerate_pixels: bool = False,
     shape: Optional[Tuple[int, int]] = None,
     ax: Optional[Axis] = None
-) -> Axis:
+) -> Union[Axis, np.ndarray]:
     """Plots binary segmentation mask."""
 
     if isinstance(mask, str):
@@ -46,10 +47,34 @@ def preview(
     ax: Optional[Axis] = None,
     overlay_config: Optional[Dict] = None,
     title: str = '',
-):
+) -> Axis:
     ax = axes(ax=ax)
     image = read_masked_image(image, mask, **(overlay_config or {}))
     ax.imshow(image)
     ax.axis('off')
     ax.set_title(title)
-    return ax
+    return cast(Axis, ax)
+
+
+def preview_arrays(
+    image: np.ndarray,
+    gt: Optional[np.ndarray] = None,
+    pred: Optional[np.ndarray] = None,
+    ax: Optional[Axis] = None,
+    overlay_config: Optional[Dict] = None,
+    title: str = '',
+):
+    ax = axes(ax=ax)
+    if image.shape[0] in (1, 3):
+        image = image.transpose((1, 2, 0))
+    overlay_config = (overlay_config or {}).copy()
+    if gt is not None:
+        overlay_config["color"] = (255, 0, 0)
+        image = overlay(image, gt, **overlay_config)
+    if pred is not None:
+        overlay_config["color"] = (0, 255, 0)
+        image = overlay(image, pred, **overlay_config)
+    ax.imshow(image)
+    ax.axis('off')
+    ax.set_title(title)
+    return cast(Axis, ax)
