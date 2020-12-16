1ee86918790d77ee3463f1c3f5580b2122b15cbb
devforfu <developer.z@outlook.com>
2020-12-16 21:51:57 +0100

    RLE-encoder utility

--- kidney/utils/mask.py
+++ kidney/utils/mask.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import numpy as np
 
@@ -22,3 +22,43 @@ def rle_decode(encoded: str, shape: Tuple[int, int]) -> np.ndarray:
     # from left to right, but the mask was filled in a different
     # order so need to transpose
     return mask.reshape(shape[1], shape[0]).T
+
+
+def rle_encode(mask: np.ndarray, threshold: Optional[float] = None) -> str:
+    """Encoders a binary mask into RLE-string.
+
+    References
+    ----------
+    [1] https://www.kaggle.com/hackerpoet/even-faster-run-length-encoder
+
+    """
+
+    pixels = mask.T.flatten()
+
+    if threshold is not None:
+        pixels = np.where(pixels > threshold, 1, 0)
+
+    pixels = pixels.astype(np.uint8)
+
+    mask_start = (pixels[:-1] == 0) & (pixels[1:] == 1)
+    mask_end = (pixels[:-1] == 1) & (pixels[1:] == 0)
+    [start_idx] = np.where(mask_start)
+    [end_idx] = np.where(mask_end)
+    lengths = end_idx - start_idx
+
+    encoded = np.zeros(start_idx.shape[0] + lengths.shape[0])
+    encoded[::2] = start_idx + 2  # adjust for counting from 1
+    encoded[1::2] = lengths
+
+    return " ".join([str(x) for x in encoded.astype(int)])
+
+
+def main():
+    mask = '9 1 16 4 24 1 39 1 46 1 51 5'
+    decoded = rle_decode(mask, (7, 9))
+    encoded = rle_encode(decoded)
+    assert mask == encoded
+
+
+if __name__ == '__main__':
+    main()
