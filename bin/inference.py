b503c8c17c43c12aced02059aecfe096d2a81bcb
devforfu <developer.z@outlook.com>
2020-12-17 20:59:30 +0100

    Inference script

---
+++ bin/inference.py
@@ -0,0 +1,54 @@
+import fnmatch
+import importlib
+import os
+from argparse import Namespace, ArgumentParser
+from multiprocessing import cpu_count
+from os.path import splitext, join
+from typing import Tuple
+
+import torch
+from zeus.utils import home
+
+from kidney.utils.checkpoints import CheckpointsStorage
+
+
+def main(args: Namespace):
+    experiment_factory = get_module_attribute(args.experiment)
+    experiment, checkpoint = CheckpointsStorage.create(experiment_factory, args.checkpoints, args.metric)
+    create_data_loaders = get_module_attribute(args.data_loaders)
+
+    meta = checkpoint.meta
+    loaders = create_data_loaders(meta["data"], meta["transformers"],
+                                  num_workers=args.num_workers,
+                                  batch_size=args.batch_size)
+
+    with torch.no_grad():
+        for batch in loaders[args.subset]:
+            outputs = experiment(batch)["outputs"]
+            outputs = meta["transformers"].post(outputs)
+
+
+def get_module_attribute(import_path: str):
+    module_path, attr = import_path.rsplit(".", 1)
+    assert module_path and attr
+    module = importlib.import_module(module_path)
+    try:
+        return getattr(module, attr)
+    except AttributeError:
+        raise RuntimeError(f"module is not found: {import_path}")
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("-e", "--experiment", required=True)
+    parser.add_argument("-d", "--data_loaders", required=True)
+    parser.add_argument("-c", "--checkpoints", required=True)
+    parser.add_argument("-s", "--subset", default="valid")
+    parser.add_argument("-m", "--metric", default="avg_val_loss")
+    parser.add_argument("-w", "--num_workers", default=cpu_count(), type=int)
+    parser.add_argument("-b", "--batch_size", default=4, type=int)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_args())
