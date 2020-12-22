2f513c53508af346de573fa38abed0a370173a61
devforfu <developer.z@outlook.com>
2020-12-22 22:01:45 +0100

    Inference

--- bin/inference.py
+++ bin/inference.py
@@ -1,15 +1,18 @@
 import importlib
 from argparse import Namespace, ArgumentParser
 from multiprocessing import cpu_count
+from typing import Dict
 
 import pandas as pd
 import pytorch_lightning as pl
+import torch
 from pytorch_lightning.utilities import AttributeDict
 
 from kidney.datasets.kaggle import get_reader, SampleType
-from kidney.inference.inference import create_inference_instance, InferenceAlgorithm
+from kidney.inference.inference import SlidingWindow, SlidingWindowConfig
 from kidney.parameters import as_attribute_dict, requires
 from kidney.utils.checkpoints import CheckpointsStorage
+from kidney.utils.image import scale_intensity_tensor
 from kidney.utils.mask import rle_encode
 
 
@@ -27,29 +30,47 @@ def main(args: Namespace):
     "subset",
     "validation",
     "roi_size",
+    "overlap",
     "batch_size"
 ])
 def inference(args: AttributeDict):
     experiment_factory = get_module_attribute(args.experiment)
     experiment, checkpoint = CheckpointsStorage.create(experiment_factory, args.checkpoints, args.metric)
-    create_data_loaders = get_module_attribute(args.data_loaders)
-
     meta = checkpoint.meta
     transformers = meta["transformers"]
-    loaders = create_data_loaders(meta["data"], transformers,
-                                  num_workers=args.num_workers,
-                                  batch_size=args.batch_size)
 
     if args.validation:
+        create_data_loaders = get_module_attribute(args.data_loaders)
+        loaders = create_data_loaders(
+            meta["data"], transformers,
+            num_workers=args.num_workers,
+            batch_size=args.batch_size)
         trainer = pl.Trainer(gpus=1)
         trainer.test(experiment, test_dataloaders=loaders[args.subset])
+
     else:
-        predictor = create_inference_instance(
-            algorithm=InferenceAlgorithm.Default,
-            experiment=experiment,
-            device=args.device,
-            transformers=transformers,
-            check_for_outliers=False
+        def transform_input(t: torch.Tensor) -> torch.Tensor:
+            t = torch.as_tensor(t)
+            t = t.float()
+            t = scale_intensity_tensor(t)
+            return t
+
+        def transform_output(output: Dict) -> Dict:
+            transformed = transformers.post(output["outputs"])
+            output.update({"outputs": transformed.squeeze()})
+            return output
+
+        predictor = SlidingWindow(
+            model=experiment,
+            device=torch.device(args.device),
+            config=SlidingWindowConfig(
+                window_size=args.roi_size,
+                overlap=args.overlap,
+                max_batch_size=args.batch_size,
+                transform_input=transform_input,
+                transform_output=transform_output,
+                check_for_outliers=False,
+            )
         )
         reader = get_reader()
         test_keys = reader.get_keys(SampleType.Unlabeled)
@@ -82,10 +103,11 @@ def parse_args() -> Namespace:
     parser.add_argument("-m", "--metric", default="avg_val_loss")
     parser.add_argument("-w", "--num_workers", default=cpu_count(), type=int)
     parser.add_argument("-b", "--batch_size", default=4, type=int)
-    parser.add_argument("-r", "--roi_size", default=1024, type=int)
     parser.add_argument("-dev", "--device", default="cuda:1")
-    parser.add_argument("--validation", action="store_true")
     parser.add_argument("-o", "--output_file", default="submission.csv")
+    parser.add_argument("--roi_size", default=1024, type=int)
+    parser.add_argument("--overlap", default=64, type=int)
+    parser.add_argument("--validation", action="store_true")
     return parser.parse_args()
 
 
--- kidney/inference/inference.py
+++ kidney/inference/inference.py
@@ -1,8 +1,9 @@
 import abc
+import itertools
 from collections import Callable
 from dataclasses import dataclass
 from enum import auto, Enum
-from typing import Optional, Tuple, Dict, Any, Union
+from typing import Optional, Tuple, Dict, Any, Union, List
 
 import cv2 as cv
 import numpy as np
@@ -14,133 +15,95 @@ from zeus.torch_tools.utils import to_np
 
 from kidney.datasets.kaggle import outlier, get_reader
 from kidney.parameters import get_relevant_params
-from kidney.utils.image import scale_intensity_tensor, channels_first
-
-DEFAULT_WINDOW_SIZE = 1024
-DEFAULT_OVERLAP = 64
+from kidney.utils.image import scale_intensity_tensor, channels_first, channels_last
 
 
-class InferenceAlgorithm(Enum):
-    Default = auto()
-
-
-class InferenceProtocol:
+class InferenceAlgorithm:
 
     @abc.abstractmethod
     def predict_from_file(self, filename: str) -> np.ndarray:
         pass
 
 
-def create_inference_instance(
-    algorithm: InferenceAlgorithm,
-    experiment: pl.LightningModule,
-    **config
-) -> InferenceProtocol:
-    config = config or {}
-
-    if algorithm == InferenceAlgorithm.Default:
-        device = torch.device(config.get("device", "cpu"))
-        experiment = experiment.to(device)
-        transformers = config.get("transformers")
-
-        def transform(tensor: Union[np.ndarray, torch.Tensor]) -> Dict:
-            nonlocal device
-            tensor = torch.as_tensor(tensor)
-            tensor = scale_intensity_tensor(tensor)
-            tensor = tensor.to(device)
-            return {"img": tensor}
-
-        def postprocessing(model_output: Dict) -> np.ndarray:
-            nonlocal transformers
-            outputs = model_output["outputs"]
-            if transformers is not None:
-                outputs = transformers.post(outputs)
-            return to_np(outputs.squeeze())
-
-        predictor = SlidingWindowPredictor(
-            model=experiment,
-            transform=transform,
-            post_model=postprocessing,
-            **get_relevant_params(SlidingWindowPredictor, config)
-        )
-
-        return predictor
-
-    raise NotImplementedError(f"unknown algorithm: {algorithm}")
-
-
-
-
-
+@dataclass
+class SlidingWindowConfig:
+    window_size: int
+    overlap: int
+    max_batch_size: int
+    check_for_outliers: bool = True
+    transform_input: Optional[Callable] = None
+    transform_output: Optional[Callable] = None
 
 
 @dataclass
-class SlidingWindowPredictor(InferenceProtocol):
-    model: Callable
-    transform: Optional[Callable] = None
-    post_model: Optional[Callable] = None
-    check_for_outliers: bool = False
-    window_size: int = DEFAULT_WINDOW_SIZE
-    overlap: int = DEFAULT_OVERLAP,
-    max_batch_size: int = 16
-
-    def __post_init__(self):
-        self.sliding_window_config = self.sliding_window_config or {}
+class SlidingWindow(InferenceAlgorithm):
+    model: pl.LightningModule
+    config: SlidingWindowConfig
+    device: torch.device
 
     def predict_from_file(self, filename: str) -> np.ndarray:
+        size = self.config.window_size
+
         with rasterio.open(filename) as dataset:
             w, h = dataset.shape
-            boxes = sliding_window_boxes(w, h, self.window_size, self.overlap)
-            n_splits = int(np.ceil(boxes.shape[0] / self.max_batch_size))
-            prediction = np.zeros((h, w), dtype=np.uint8)
+            boxes = sliding_window_boxes(w, h, size, self.config.overlap)
+            n_splits = int(np.ceil(boxes.shape[0] / self.config.max_batch_size))
+            mask = np.zeros((h, w), dtype=np.uint8)
 
             for batch in np.array_split(boxes, n_splits):
-                images = [
-                    self.transform(image)
-                    for image in (
-                        dataset.read(
-                            [1, 2, 3],
-                            window=Window.from_slices((x1, x2), (y1, y2))
+                samples = [
+                    {
+                        "img": self.transform_input(
+                            channels_first(
+                                cv.resize(
+                                    channels_last(image).astype(np.float32),
+                                    (size, size)
+                                )
+                            )
+                        ),
+                        "box": box
+                    }
+                    for box, image in (
+                        (
+                            [x1, y1, x2, y2],
+                            dataset.read(
+                                [1, 2, 3],
+                                window=Window.from_slices((x1, x2), (y1, y2))
+                            )
                         )
                         for x1, y1, x2, y2 in batch
                     )
                     if (
-                        not self.check_for_outliers or
-                        self.check_for_outliers and not outlier(image)
+                        not self.config.check_for_outliers or
+                        self.config.check_for_outliers and not outlier(image)
                     )
                 ]
+                if not samples:
+                    continue
+                collated = self.collate_batch(samples)
+                output = self.model(collated)
+                result = self.transform_output(output)
+                self.update_predictions_mask(mask, result, collated["meta"])
 
+        return mask
 
-        return np.zeros((4, 4))
+    def transform_input(self, t: Union[np.ndarray, torch.Tensor]) -> Dict:
+        return t if self.config.transform_input is None else self.config.transform_input(t)
 
-    # def predict_from_file(self, filename: str):
-    #     dataset = rasterio.open(filename)
-    #     w, h = dataset.shape
-    #     boxes = sliding_window_boxes((h, w), **self.sliding_window_config)
-    #     prediction = np.zeros(dataset.shape, dtype=np.uint8)
-    #     for x1, y1, x2, y2 in boxes:
-    #         window = Window.from_slices((x1, x2), (y1, y2))
-    #         image = dataset.read([1, 2, 3], window=window)
-    #         shape = (y2 - y1, x2 - x1)
-    #         if self.check_for_outliers and outlier(image):
-    #             mask = np.zeros(shape, dtype=np.uint8)
-    #         else:
-    #             image = np.moveaxis(image, 0, -1)
-    #             image = cv.resize(image, shape)
-    #             image = channels_first(image)
-    #             image = image[np.newaxis]
-    #             batch = self._transform_image(image)
-    #             with torch.no_grad():
-    #                 mask = self._get_scores_from_model(self.model(batch))
-    #                 print(mask.shape)
-    #         prediction[y1:y2, x1:x2] = mask
-    #     return prediction
+    def transform_output(self, x: Dict) -> Dict:
+        return x if self.config.transform_output is None else self.config.transform_output(x)
 
-    def _transform_image(self, image: np.ndarray) -> torch.Tensor:
-        return image if self.transform is None else self.transform(image)
+    @classmethod
+    def collate_batch(cls, samples: List[Dict]) -> Dict:
+        img = torch.stack([sample["img"] for sample in samples])
+        meta = [{k: v for k, v in sample.items() if k != "img"} for sample in samples]
+        return {"img": img, "meta": meta}
 
-    def _get_scores_from_model(self, output: Any) -> torch.Tensor:
-        return output if self.post_model is None else self.post_model(output)
+    @classmethod
+    def update_predictions_mask(cls, mask: np.ndarray, result: Dict, meta: List[Dict]):
+        for i, info in enumerate(meta):
+            x1, y1, x2, y2 = info["box"]
+            mask[y1:y2, x1:x2] = to_np(result["outputs"][i].byte())
 
 
 def sliding_window_boxes(
