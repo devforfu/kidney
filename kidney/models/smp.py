9c2f0546d90a212acbbdba82427fb85e8243030b
devforfu <developer.z@outlook.com>
2021-01-15 20:16:07 +0100

    Unet++ supported

--- kidney/models/smp.py
+++ kidney/models/smp.py
@@ -1,8 +1,15 @@
+from typing import Any, Dict
+
 import segmentation_models_pytorch as smp
 import torch.nn as nn
 from pytorch_lightning.utilities import AttributeDict
 
 from kidney.parameters import get_relevant_params, requires
+
+_MODEL_FACTORY = {
+    "unet": smp.Unet,
+    "unet++": smp.UnetPlusPlus,
+}
 
 
 @requires([
@@ -10,8 +17,15 @@ from kidney.parameters import get_relevant_params, requires
     "smp_model_config"
 ])
 def create_smp_model(params: AttributeDict) -> nn.Module:
-    if params.smp_model_type == "unet":
-        unet_params = get_relevant_params(smp.Unet.__init__, params.smp_model_config)
-        return smp.Unet(**unet_params)
-    else:
-        raise ValueError(f"unknown model type: {params.smp_model_type}")
+    model_name = params.smp_model_type.lower()
+    model_cls = key_or_raise(_MODEL_FACTORY, model_name)
+    params = get_relevant_params(model_cls.__init__, params.smp_model_config)
+    model = model_cls(**params)
+    return model
+
+
+def key_or_raise(d: Dict, key: str) -> Any:
+    if key not in d:
+        keys = sorted(list(d.keys()))
+        raise KeyError(f"key is not found: {key}; available keys are: {keys}")
+    return d[key]
