72135cb67895bf89c70a4fee1c4ecfdb1d466c5a
devforfu <developer.z@outlook.com>
2021-01-17 22:55:24 +0100

    Masks report

--- kidney/reports/diff.py
+++ kidney/reports/diff.py
@@ -1,6 +1,6 @@
 import os
 
-import PIL.Image
+import cv2 as cv
 import numpy as np
 import streamlit as st
 
@@ -13,14 +13,20 @@ from kidney.utils.image import overlay_masks
 
 session_state = session.get(password=False)
 
+PREDICTIONS_DIR = os.path.join(os.environ["DATASET_ROOT"], "predictions")
 
-def read_prediction_mask(sample_key: str) -> np.ndarray:
-    root = os.path.join(os.environ["DATASET_ROOT"], "predictions")
+
+def select_model_dir():
+    st.subheader("Select predictions")
+    model_dir = st.selectbox(label="Model", options=os.listdir(PREDICTIONS_DIR))
+    return os.path.join(PREDICTIONS_DIR, model_dir)
+
+
+def read_prediction_mask(root: str, sample_key: str) -> np.ndarray:
     mask_path = os.path.join(root, f"{sample_key}.png")
     if not os.path.exists(mask_path):
         return None
-    image = PIL.Image.open(mask_path)
-    return np.asarray(image)
+    return (cv.imread(mask_path, 0) == 255).astype(np.uint8)
 
 
 @with_password(session_state)
@@ -31,19 +37,26 @@ def main():
 
     meta = reader.fetch_meta(sample_key)
     image, info = read_image(meta, thumb_size, overlay_mask=False)
+
     masks = []
 
-    mask_pred = read_prediction_mask(sample_key)
-    if mask_pred is not None:
-        color = st.color_picker(label="Prediction mask color", value="#0000ff")
-        masks.append((mask_pred, hex_to_color(color)))
+    mask_pred = read_prediction_mask(select_model_dir(), sample_key)
 
-    if info.get("mask") is not None:
-        color = st.color_picker(label="Ground Truth mask color", value="#ff0000")
-        masks.append((info["mask"], hex_to_color(color)))
+    if st.checkbox(label="Show Prediction", value=True):
+        if mask_pred is not None:
+            mask_pred = cv.resize(mask_pred, image.shape[:2])
+            color = st.color_picker(label="Prediction mask color", value="#0000ff")
+            masks.append((mask_pred, hex_to_color(color)))
 
-    image_with_overlay = overlay_masks(image.copy(), masks, convert_to_uint=False)
-    st.image(image_with_overlay, caption="Image with mask(s)")
+    if st.checkbox(label="Show Ground Truth", value=True):
+        if info.get("mask") is not None:
+            color = st.color_picker(label="Ground Truth mask color", value="#ff0000")
+            masks.append((info["mask"], hex_to_color(color)))
+
+    if masks:
+        image = overlay_masks(image.copy(), masks, convert_to_uint=False)
+
+    st.image(image, caption="Image with mask(s)")
 
 
 if __name__ == '__main__':
