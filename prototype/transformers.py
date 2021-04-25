02c3814721f4558cc68cc9073c256bcd4c326edb
devforfu <developer.z@outlook.com>
2021-04-25 15:23:32 +0200

    Support aug disabling

--- prototype/transformers.py
+++ prototype/transformers.py
@@ -20,76 +20,79 @@ def create_transformers(
 
     train_steps, valid_steps = [], []
 
-    image_size = config.image_size
-    if image_size is not None:
-        train_steps.append(A.Resize(image_size, image_size))
-        valid_steps.append(A.Resize(image_size, image_size))
+    if config.resize:
+        image_size = config.image_size
+        if image_size is not None:
+            train_steps.append(A.Resize(image_size, image_size))
+            valid_steps.append(A.Resize(image_size, image_size))
 
-    if config.bcg is not None:
-        bcg = config.bcg
-        train_steps.append(
-            A.OneOf(
-                [
-                    A.RandomBrightnessContrast(bcg.brightness, bcg.contrast, p=1),
-                    A.RandomGamma(bcg.gamma, p=1),
-                ],
-                p=bcg.prob,
+    if not config.normalize_only:
+
+        if config.bcg is not None:
+            bcg = config.bcg
+            train_steps.append(
+                A.OneOf(
+                    [
+                        A.RandomBrightnessContrast(bcg.brightness, bcg.contrast, p=1),
+                        A.RandomGamma(bcg.gamma, p=1),
+                    ],
+                    p=bcg.prob,
+                )
             )
-        )
 
-    if config.blur is not None:
-        blur = config.blur
-        train_steps.append(
-            A.OneOf(
-                [A.Blur(blur_limit=blur.limit), A.MedianBlur(blur_limit=blur.limit)],
-                p=blur.prob,
+        if config.blur is not None:
+            blur = config.blur
+            train_steps.append(
+                A.OneOf(
+                    [A.Blur(blur_limit=blur.limit), A.MedianBlur(blur_limit=blur.limit)],
+                    p=blur.prob,
+                )
             )
-        )
 
-    if config.noise is not None:
-        noise = config.noise
-        train_steps.append(
-            A.OneOf(
-                [A.GaussNoise(var_limit=noise.gauss_var)]
-                if noise.affine_scale is None
-                else [
-                    A.GaussNoise(var_limit=noise.gauss_var),
-                    A.IAAAffine(scale=noise.affine_scale),
-                ],
-                p=noise.prob,
+        if config.noise is not None:
+            noise = config.noise
+            train_steps.append(
+                A.OneOf(
+                    [A.GaussNoise(var_limit=noise.gauss_var)]
+                    if noise.affine_scale is None
+                    else [
+                        A.GaussNoise(var_limit=noise.gauss_var),
+                        A.IAAAffine(scale=noise.affine_scale),
+                    ],
+                    p=noise.prob,
+                )
             )
-        )
 
-    if config.flip is not None:
-        flip = config.flip
-        flips = []
-        if flip.vertical:
-            flips.append(A.VerticalFlip(p=flip.prob))
-        if flip.horizontal:
-            flips.append(A.HorizontalFlip(p=flip.prob))
-        train_steps.extend(flips)
+        if config.flip is not None:
+            flip = config.flip
+            flips = []
+            if flip.vertical:
+                flips.append(A.VerticalFlip(p=flip.prob))
+            if flip.horizontal:
+                flips.append(A.HorizontalFlip(p=flip.prob))
+            train_steps.extend(flips)
 
-    if config.elastic is not None:
-        elastic = config.elastic
-        train_steps.append(
-            A.ElasticTransform(
-                alpha=elastic.alpha,
-                sigma=elastic.sigma,
-                alpha_affine=elastic.alpha_affine,
-                p=elastic.prob,
+        if config.elastic is not None:
+            elastic = config.elastic
+            train_steps.append(
+                A.ElasticTransform(
+                    alpha=elastic.alpha,
+                    sigma=elastic.sigma,
+                    alpha_affine=elastic.alpha_affine,
+                    p=elastic.prob,
+                )
             )
-        )
 
-    if config.ssr is not None:
-        ssr = config.ssr
-        train_steps.append(
-            A.ShiftScaleRotate(
-                shift_limit=ssr.shift,
-                scale_limit=ssr.scale,
-                rotate_limit=ssr.rotate,
-                p=ssr.prob,
+        if config.ssr is not None:
+            ssr = config.ssr
+            train_steps.append(
+                A.ShiftScaleRotate(
+                    shift_limit=ssr.shift,
+                    scale_limit=ssr.scale,
+                    rotate_limit=ssr.rotate,
+                    p=ssr.prob,
+                )
             )
-        )
 
     final_step = (
         [A.NoOp()]
